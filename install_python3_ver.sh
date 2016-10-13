41d668a32a771e4e128b499908ec4e3ca6fd7071
zweiein <einphm@gmail.com>
2016-10-13 14:42:46 +0800

    how to install python3 on Ubuntu

---
+++ install_python3_ver.sh
@@ -0,0 +1,12 @@
+#Strongly recommend you install it after setting virtualenv!!!
+
+# install python3 version, the most important thing is the PIP3
+sudo apt-get install -y python3-numpy python3-scipy python3-nose python3-h5py python3-skimage python3-matplotlib python3-pandas python3-sklearn python3-sympy
+sudo apt-get install -y python3-dev python3-pip g++ python3-pygments python3-sphinx libjpeg-dev zlib1g-dev
+
+sudo pip3 install matplotlib ipython[all] jupyter pandas scikit-image
+sudo pip3 install theano
+sudo pip3 install keras
+
+# keras needs scipy!
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
