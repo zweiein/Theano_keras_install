9b1aad9884f8e88cf1d6655cb1d7c67af7f5647b
zweiein <einphm@gmail.com>
2016-10-13 15:03:37 +0800

    fixed bugs

--- install_python3_ver.sh
+++ install_python3_ver.sh
@@ -4,8 +4,9 @@
 sudo apt-get install -y python3-numpy python3-scipy python3-nose python3-h5py python3-skimage python3-matplotlib python3-pandas python3-sklearn python3-sympy
 sudo apt-get install -y python3-dev python3-pip g++ python3-pygments python3-sphinx libjpeg-dev zlib1g-dev
 
-sudo pip3 install matplotlib ipython[all] jupyter pandas scikit-image
-sudo pip3 install theano
+# sudo pip3 install matplotlib ipython[all] jupyter pandas scikit-image
+# sudo pip3 install theano 
+
 sudo pip3 install keras
 
 # keras needs scipy!
